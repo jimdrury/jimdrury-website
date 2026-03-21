ffdabb51e8f7475ac2fe93a9b1fe21fab35004de
Jim Drury <jim@jimdrury.co.uk>
2026-03-21 23:00:07 +0000

    fix: hydration warning

--- src/bloks/RichTextBlok.tsx
+++ src/bloks/RichTextBlok.tsx
@@ -23,6 +23,7 @@ export const RichTextBlok: FC<RichTextBlokProps> = ({ blok }) => {
   return (
     <div
       {...storyblokEditable(blok)}
+      suppressHydrationWarning
       className="prose text-pretty richtext-external-link-indicator [&_h1]:text-balance [&_h2]:text-balance [&_h3]:text-balance [&_h4]:text-balance [&_li>p:first-child]:mt-0 [&_li>p:last-child]:mb-0 [&_ul>li::marker]:text-black [&_:not(pre)>code]:rounded [&_:not(pre)>code]:bg-black [&_:not(pre)>code]:px-1.5 [&_:not(pre)>code]:py-0.5 [&_:not(pre)>code]:font-bold [&_:not(pre)>code]:text-yellow-400 [&_:not(pre)>code]:whitespace-nowrap [&_:not(pre)>code]:before:content-none [&_:not(pre)>code]:after:content-none"
     >
       <StoryblokServerRichText doc={blok.content} />
